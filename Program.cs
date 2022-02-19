2851abf263a3ce61961c36a8f964f9909f8c2428
dhruvpatel7014 <100025899+dhruvpatel7014@users.noreply.github.com>
2022-02-19 17:24:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace practicle2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TRANSPORTATION SYSTEM");
+            Console.WriteLine("Enter the Distance:");
+           int dist=Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("Enter the weight of goods in Kg:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            int cost;
+            if(dist<100)
+            {
+                cost = 50 *weight;
+
+            }
+            else if(dist<200)
+            {
+                cost = 65 * weight;
+            }
+            else if (dist < 300)
+            {
+                cost = 90 * weight;
+            }
+            else
+            {
+                cost = 120 * weight;
+            }
+            Console.WriteLine($"Total cost of goods is{cost}");
+            Console.ReadLine();
+
+        }
+    }
+}
